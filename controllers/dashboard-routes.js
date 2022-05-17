715cffe4c3e4b30a703918d1ec2829c76ccb8a14
llattanzi <lauren71442@gmail.com>
2022-05-17 11:40:31 -0400

    add home and dashboard routes

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,16 +1,42 @@
 const router = require('express').Router();
-const { Post } = require('../models/');
+const { Post, User, Comment } = require('../models/');
 const withAuth = require('../utils/auth');
 
+// Get all posts
 router.get('/', withAuth, async (req, res) => {
   try {
     // store the results of the db query in a variable called postData. should use something that "finds all" from the Post model. may need a where clause!
+    postData = await Post.findAll({
+      where: {
+        user_id: req.session.user_id
+      },
+      attributes: [
+        'id',
+        'post_url',
+        'title',
+        'created_at'
+      ],
+      include: [
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        },
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
+    })
 
     // this sanitizes the data we just got from the db above (you have to create the above)
     const posts = postData.map((post) => post.get({ plain: true }));
 
     // fill in the view to be rendered
-    res.render('hmmmm what goes here', {
+    res.render('all-posts', {
       // this is how we specify a different layout other than main! no change needed
       layout: 'dashboard',
       // coming from line 10 above, no change needed
@@ -21,9 +47,38 @@ router.get('/', withAuth, async (req, res) => {
   }
 });
 
+// get single post
+router.get('/post/:id', async (req, res) => {
+  try {
+    // what should we pass here? we need to get some data passed via the request body (something.something.id?)
+    // change the model below, but not the findByPk method.
+    const postData = await Post.findByPk(req.params.id, {
+      // helping you out with the include here, no changes necessary
+      include: [
+        User,
+        {
+          model: Comment,
+          include: [User],
+        },
+      ],
+    });
+
+    if (postData) {
+      // serialize the data
+      const post = postData.get({ plain: true });
+      // which view should we render for a single-post?
+      res.render('single-post', { post });
+    } else {
+      res.status(404).end();
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 router.get('/new', withAuth, (req, res) => {
   // what view should we send the client when they want to create a new-post? (change this next line)
-  res.render('hmmmm what goes here', {
+  res.render('new-post', {
     // again, rendering with a different layout than main! no change needed
     layout: 'dashboard',
   });
@@ -32,13 +87,13 @@ router.get('/new', withAuth, (req, res) => {
 router.get('/edit/:id', withAuth, async (req, res) => {
   try {
     // what should we pass here? we need to get some data passed via the request body
-    const postData = await Post.findByPk(????);
+    const postData = await Post.findByPk(req.params.id);
 
     if (postData) {
       // serializing the data
       const post = postData.get({ plain: true });
       // which view should we render if we want to edit a post?
-      res.render('hmmmm what goes here', {
+      res.render('edit-post', {
         layout: 'dashboard',
         post,
       });
