ebf1596c7368f743f84ab624d995760721b19de3
beatricekallen <bkallen90@gmail.com>
2022-05-19 14:21:09 -0400

    updates post and comment routes

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,52 +1,57 @@
-const router = require('express').Router();
-const { Post } = require('../../models/');
-const withAuth = require('../../utils/auth');
+const router = require("express").Router();
+const { Post } = require("../../models");
+const withAuth = require("../../utils/auth");
 
-router.post('/', withAuth, async (req, res) => {
+router.post("/", withAuth, async (req, res) => {
   const body = req.body;
 
   try {
-    const newPost = await Post.create({ ...body, userId: req.session.userId });
+    const newPost = await Post.create({
+      title: req.body.title,
+      body: req.body.body,
+      birds: req.body.birds,
+      user_id: req.session.user_id,
+    });
     res.json(newPost);
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.put('/:id', withAuth, async (req, res) => {
-  try {
-    const [affectedRows] = await Post.update(req.body, {
-      where: {
-        id: req.params.id,
-      },
-    });
+// router.put("/:id", withAuth, async (req, res) => {
+//   try {
+//     const [affectedRows] = await Post.update(req.body, {
+//       where: {
+//         id: req.params.id,
+//       },
+//     });
 
-    if (affectedRows > 0) {
-      res.status(200).end();
-    } else {
-      res.status(404).end();
-    }
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+//     if (affectedRows > 0) {
+//       res.status(200).end();
+//     } else {
+//       res.status(404).end();
+//     }
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
 
-router.delete('/:id', withAuth, async (req, res) => {
-  try {
-    const [affectedRows] = Post.destroy({
-      where: {
-        id: req.params.id,
-      },
-    });
+// router.delete("/:id", withAuth, async (req, res) => {
+//   try {
+//     const [affectedRows] = Post.destroy({
+//       where: {
+//         id: req.params.id,
+//       },
+//     });
 
-    if (affectedRows > 0) {
-      res.status(200).end();
-    } else {
-      res.status(404).end();
-    }
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+//     if (affectedRows > 0) {
+//       res.status(200).end();
+//     } else {
+//       res.status(404).end();
+//     }
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
 
 module.exports = router;
