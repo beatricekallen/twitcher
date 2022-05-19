e4fe0fec6218ec51f3c3938bb519cd32c218d112
beatricekallen <bkallen90@gmail.com>
2022-05-19 18:09:32 -0400

    sets up hero

--- public/js/script.js
+++ public/js/script.js
@@ -1,9 +1,8 @@
-// Show scroll up 
-function scrollUp () {
-    const scrollUp = document.getElementById ('scroll-up');
-    // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the
-    if (this.scrollY >= 560) scrollUp.classList.add ('show-scroll'); else scrollUp.classList.remove ('show-scroll') 
+// Show scroll up
+function scrollUp() {
+  const scrollUp = document.getElementById("scroll-up");
+  // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the
+  if (this.scrollY >= 560) scrollUp.classList.add("show-scroll");
+  else scrollUp.classList.remove("show-scroll");
 }
-window.addEventListener ('scroll', scrollUp)
-
-	
+window.addEventListener("scroll", scrollUp);
