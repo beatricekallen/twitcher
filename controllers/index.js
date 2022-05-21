ddb0c398ff3822d3c11d7a8e8129fcd19aa52d58
beatricekallen <bkallen90@gmail.com>
2022-05-21 12:07:28 -0400

    creates blog routes

--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,10 @@ const router = require("express").Router();
 
 const apiRoutes = require("./api/");
 const homeRoutes = require("./home-routes.js");
+const blogRoutes = require("./blog-routes.js");
 
 router.use("/", homeRoutes);
 router.use("/api", apiRoutes);
+router.use("/blog", blogRoutes);
 
 module.exports = router;
