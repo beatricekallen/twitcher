c1801e34ee602ad97a303882d9729ee3921a0ab0
brittanymy <brittany.young2001@gmail.com>
2022-05-22 18:07:51 -0400

    mapped out botd in js with comments

--- public/js/script.js
+++ public/js/script.js
@@ -1,8 +1,24 @@
-// Show scroll up
-function scrollUp() {
-  const scrollUp = document.getElementById("scroll-up");
-  // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the
-  if (this.scrollY >= 560) scrollUp.classList.add("show-scroll");
-  else scrollUp.classList.remove("show-scroll");
-}
-window.addEventListener("scroll", scrollUp);
+// Reference HTML elements 
+const birdName = document.getElementById("bird-name"),
+   birdDescription = document.getElementById("bird-description"),
+   birdImage = document.getElementById("bird-img")
+
+// Declare variables that will be filled by bird APIS
+let birdCommonName = " ",
+   birdScientificName = " ",
+   birdFamily = " "
+
+// eBird API key
+let apiKey = "96kajpv2hctt"
+// eBird API url
+let eBirdURL = "https://api.ebird.org/v2/ref/taxonomy/ebird"
+// eBird random birl url
+let randomBirdURL = "https://ebird.org/species/surprise-me"
+// google search API url
+let googleSearchURL = "https://customsearch.googleapis.com/customsearch/v1"
+
+// Function that extracts an array of data from eBird API
+
+// Function that extracts link from google API response based off of common name
+
+// Function that implements API data into html elements
