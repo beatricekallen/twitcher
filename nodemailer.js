f9c8aadc786b5f339be6ebdb04cc6eed77ef3d9e
beatricekallen <bkallen90@gmail.com>
2022-05-23 22:40:17 -0400

    updates nodemailer email

--- nodemailer.js
+++ nodemailer.js
@@ -31,8 +31,9 @@ const sendEmail = async () => {
     let mailOptions = {
       from: "triangle.twitchers@gmail.com",
       to: [...mailingList, "ocskier@gmail.com"],
-      subject: "Nodemailer Project",
-      text: "Hi from your nodemailer project",
+      subject: "Update from Triangle Twitchers!",
+      text: "Hello, fellow birdwatcher! It's officially summer here in North Carolina, which means the start of all sorts of exciting bird activity. The sight of ruby-throated hummingbirds descending on feeders and flowers alike never fails to delight. We have plenty of exciting events planned in the coming months, so check out our events page and stay in touch! If you're a new member, we'd love to hear about your birding experiences on our blog page. Thanks for being a member of the Triangle Twitcher community! Happy birding, --The Triangle Twitcher Team",
+      html: "<h3>Hello, fellow birdwatcher!</h3> <p>It's officially summer here in North Carolina, which means the start of all sorts of exciting bird activity. The sight of ruby-throated hummingbirds descending on feeders and flowers alike never fails to delight. We have plenty of exciting events planned in the coming months, so check out our events page and stay in touch! If you're a new member, we'd love to hear about your birding experiences on our blog page. Thanks for being a member of the Triangle Twitcher community! </p> <br/><p>Happy birding,</p> <br/> <p>--The Triangle Twitcher Team</p>",
     };
 
     transporter.sendMail(mailOptions, function (err, data) {
