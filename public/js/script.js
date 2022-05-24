44c6e55e0650af192c43e8bee68fb2a862a3c7f8
brittanymy <brittany.young2001@gmail.com>
2022-05-24 12:42:17 -0400

    added botd functionality

--- public/js/script.js
+++ public/js/script.js
@@ -20,15 +20,45 @@ const googleKey = "AIzaSyAjCiOUt3sF9XYeGSGK9N1DKYZPGj-is7c"
 const googleSearchURL = "https://customsearch.googleapis.com/customsearch/v1"
 
 // Function that extracts an array of data from eBird API
-let getBirdData = function () {
+const getBirdData = function () {
    let eBirdURL = "https://api.ebird.org/v2/ref/taxonomy/ebird?fmt=json&cat=species"
 
    fetch(eBirdURL)
-     .then(function (response) {
-       if (response.ok) {
-         response.json().then(function (data) {
-           let birdIndex = Math.floor(Math.random() * data.length);
-           console.log(data[birdIndex]);
-           birdCommonName = data[birdIndex].comName
-           birdScientificName = data[birdIndex].sciName
-           birdFamily = data[birdIndex].familyComName
+     .then( response => response.json())
+     .then( data => {
+       //console.log(data)
+       var randomNum = Math.floor(Math.random() * 1000);
+       var selectedBird = data[randomNum]
+       //console.log(selectedBird)
+       birdName.textContent = selectedBird.comName
+       birdDescription.textContent = selectedBird.familyComName + ", " + selectedBird.sciName
+       return selectedBird
+     }).then(res => {
+      console.log(res)
+      fetch(`https://www.googleapis.com/customsearch/v1?key=AIzaSyAjCiOUt3sF9XYeGSGK9N1DKYZPGj-is7c&cx=c5dfc36930162300f&q=${res.comName}`)
+        .then(data => data.json())
+        .then(gresponse => {
+          console.log(gresponse)
+          birdImage.setAttribute("src", gresponse.items[0].pagemap.cse_image[0].src)
+          // const src = response.items[0].pagemap.cse_image[0].src;
+          // const img = `<img src=${src} />`
+          // contentDivEl.innerHTML = img;
+        })
+     })
+    }     
+      
+    //   function (response) {
+    //    if (response.ok) {
+    //      response.json().then(function (data) {
+    //        let birdIndex = Math.floor(Math.random() * data.length);
+    //        console.log(data[birdIndex]);
+    //        birdCommonName = data[birdIndex].comName
+    //        birdScientificName = data[birdIndex].sciName
+    //        birdFamily = data[birdIndex].familyComName
+    //      }
+    //  )}
+      
+
+
+
+    getBirdData()
